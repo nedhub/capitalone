9131aa95aa6b3e1a849d43aa2209f2dc869dc4da
Ned Ememanka <officialchinedu@gmail.com>
2022-01-11 16:40:14 -0500

    commit

---
+++ arcade/adjacentElementsProduct.js
@@ -0,0 +1,21 @@
+
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+// Example
+
+// For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+// solution(inputArray) = 21.
+
+// 7 and 3 produce the largest product.
+
+
+
+function solution(inputArray) {
+    let max = -Infinity;
+        for (let i = 1; i < inputArray.length; i++) {
+            max = Math.max(inputArray[i] * inputArray[i - 1], max);
+        }
+    
+        return max;
+    }
+    
