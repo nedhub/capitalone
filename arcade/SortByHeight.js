d3ae9df8a23f34246eef896492f80dd7e4283a52
Ned Ememanka <officialchinedu@gmail.com>
2022-01-11 19:02:46 -0500

    commit

---
+++ arcade/SortByHeight.js
@@ -0,0 +1,16 @@
+function solution(a) {
+
+    const sortedA = [...a].sort((a, b) => a - b).filter((num) => num !== -1);
+    let sortedAIndex = 0;
+    for (let i = 0; i < a.length; i++) {
+    
+    if (a[i] === -1) continue;
+    else {
+    
+    a[i] = sortedA[sortedAIndex++];
+    }
+    }
+    return a;
+    }
+
+    
