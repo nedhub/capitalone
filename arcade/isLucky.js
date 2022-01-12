261301a42ee476a7ddc796a97164578501197c9d
Ned Ememanka <officialchinedu@gmail.com>
2022-01-11 19:00:03 -0500

    commit

---
+++ arcade/isLucky.js
@@ -0,0 +1,11 @@
+function solution(n) {    
+    let count = 0;
+    n = String(n).split('').map(t => {return parseInt(t)});
+    n.forEach( (el, i) => { (i < n.length / 2) ? count += el : count -= el });
+    return count == 0;
+}
+
+
+// Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+// Given a ticket number n, determine if it's lucky or not.
